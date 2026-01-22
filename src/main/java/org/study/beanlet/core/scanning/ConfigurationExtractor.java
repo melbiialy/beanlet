16652edc514490afc19126506ed438a0e79faabc
Elbialy0 <mahmoudelbialy109@gmail.com>
2026-01-22 20:58:00 +0200

    Introduce `ConfigurationExtractor` to extract bean definitions from classes annotated with `@Configuration`.

---
+++ src/main/java/org/study/beanlet/core/scanning/ConfigurationExtractor.java
@@ -0,0 +1,33 @@
+package org.study.beanlet.core.scanning;
+
+
+import org.study.beanlet.annotation.Bean;
+import org.study.beanlet.annotation.Configuration;
+import org.study.beanlet.beans.definition.BeanDefinition;
+import org.study.beanlet.beans.definition.BeanDefinitionBuilder;
+import org.study.beanlet.core.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class ConfigurationExtractor implements Extractor{
+    @Override
+    public List<BeanDefinition> extract(Class<?> clazz) {
+        List<Method> methods = ReflectionUtils.getMethods(clazz, Bean.class);
+        return methods.stream().map(method->{
+            BeanDefinitionBuilder builder = new BeanDefinitionBuilder();
+            return builder.beanClass(method.getReturnType())
+                    .scope(ReflectionUtils.getBeanScope(method.getReturnType()))
+                    .beanQualifiedName(ReflectionUtils.getBeanQualifiedName(method.getReturnType()))
+                    .lazy(ReflectionUtils.isLazy(method.getReturnType()))
+                    .primary(ReflectionUtils.isPrimary(method.getReturnType()))
+                    .factoryMethod(method)
+                    .build();
+        }).toList();
+    }
+
+    @Override
+    public boolean support(Class<?> clazz) {
+        return clazz.isAnnotationPresent(Configuration.class);
+    }
+}
